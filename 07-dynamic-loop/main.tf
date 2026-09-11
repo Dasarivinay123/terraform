8a2252ff74e962555d38ea2f8c193dac45f40769
Vinay Dasari <dasariv211@gmail.com>
2026-09-11 09:54:07 +0530

    terraform

---
+++ 07-dynamic-loop/main.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "terraform_demo" {
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_terraform.id] #list
+    tags = {
+      Name = "allow_demo-1"
+      Project = "roboshop"
+      Environment = "dev"
+  }
+
+}
+resource "aws_security_group" "allow_terraform" {
+  name        = "allow_terraform_dynamic_loop"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+ 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port        = ingress.value.port
+      to_port          = ingress.value.port
+      protocol         = "tcp"
+      cidr_blocks      = ingress.value.cidr_blocks
+    }
+  }
+  tags = {
+    Name = "allow_terraform"
+    Project = "roboshop"
+    Environment = "dev"
+  }
+}
